e9d5c8a997b5d8aa1eb051a941a6fbc7b1953fb6
M.RAY <a.mray47@yahoo.fr>
2022-02-01 22:19:33 +0800

    Add user dynamic routing

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import eventCreate from "../views/EventCreate.vue";
 import EventList from "../views/EventList.vue";
 import EventShow from "../views/EventShow.vue";
+import User from "../views/User.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/event/create",
     name: "event-create",
     component: eventCreate,
+  },
+  {
+    path: "/user/:username",
+    name: "user",
+    component: User,
   },
 ];
 
