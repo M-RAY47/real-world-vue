f8485bad9cb3f2a41fb43d95ea03624ca689bfb2
M.RAY <a.mray47@yahoo.fr>
2022-02-04 16:27:17 +0800

    Add route to eventShow

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link :to="{ name: 'event-list' }">List</router-link> |
-    <router-link :to="{ name: 'event-show' }">Event Show</router-link>
+    <router-link :to="{ name: 'event-create' }">Create</router-link>
   </div>
   <router-view />
 </template>
