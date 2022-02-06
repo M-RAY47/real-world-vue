14ec32a3d00406820036a3c02f57302e166ac197
M.RAY <a.mray47@yahoo.fr>
2022-02-06 23:16:41 +0800

    Add data to eventCard

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <navbar />
+  <navBar />
+  <router-link />
 </template>
-
+<script>
+import NavBar from '@/components/NavBar.vue';
+export default {
+  components: {
+    navBar,
+  },
+}
+</script>
 <style>
 html {
   -webkit-text-size-adjust: 100%;
@@ -10,7 +18,7 @@ html {
 }
 body {
   margin: 0;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   font-size: 16px;
   line-height: 1.5;
 }
@@ -42,7 +50,7 @@ h5,
 h6 {
   display: flex;
   align-items: center;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
 }
 h1 {
   font-size: 50px;
@@ -116,7 +124,7 @@ optgroup,
 select,
 textarea {
   display: inline-flex;
-  font-family: 'Open sans', sans-serif;
+  font-family: "Open sans", sans-serif;
   font-size: 100%;
   line-height: 1.15;
   margin: 0;
@@ -130,22 +138,22 @@ select {
   text-transform: none;
 }
 button,
-[type='button'],
-[type='reset'],
-[type='submit'] {
+[type="button"],
+[type="reset"],
+[type="submit"] {
   -webkit-appearance: none;
 }
 button::-moz-focus-inner,
-[type='button']::-moz-focus-inner,
-[type='reset']::-moz-focus-inner,
-[type='submit']::-moz-focus-inner {
+[type="button"]::-moz-focus-inner,
+[type="reset"]::-moz-focus-inner,
+[type="submit"]::-moz-focus-inner {
   border-style: none;
   padding: 0;
 }
 button:-moz-focusring,
-[type='button']:-moz-focusring,
-[type='reset']:-moz-focusring,
-[type='submit']:-moz-focusring {
+[type="button"]:-moz-focusring,
+[type="reset"]:-moz-focusring,
+[type="submit"]:-moz-focusring {
   outline: 2px solid #39b982;
 }
 label {
@@ -162,35 +170,35 @@ textarea {
   overflow: auto;
   font-size: 20px;
 }
-[type='checkbox'],
-[type='radio'] {
+[type="checkbox"],
+[type="radio"] {
   box-sizing: border-box;
   padding: 0;
 }
-[type='number']::-webkit-inner-spin-button,
-[type='number']::-webkit-outer-spin-button {
+[type="number"]::-webkit-inner-spin-button,
+[type="number"]::-webkit-outer-spin-button {
   height: auto;
 }
-[type='search'] {
+[type="search"] {
   -webkit-appearance: textfield;
   outline-offset: -2px;
 }
-[type='search']::-webkit-search-decoration {
+[type="search"]::-webkit-search-decoration {
   -webkit-appearance: none;
 }
-[type='text'],
-[type='number'],
-[type='search'],
-[type='password'] {
+[type="text"],
+[type="number"],
+[type="search"],
+[type="password"] {
   height: 52px;
   width: 100%;
   padding: 0 10px;
   font-size: 20px;
 }
-[type='text']:focus,
-[type='number']:focus,
-[type='search']:focus,
-[type='password']:focus {
+[type="text"]:focus,
+[type="number"]:focus,
+[type="search"]:focus,
+[type="password"]:focus {
   border-color: #39b982;
 }
 ::-webkit-file-upload-button {
