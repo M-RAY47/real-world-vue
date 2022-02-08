243d290051c123e2403a7c2ae2bfbdab6eb699c6
M.RAY <a.mray47@yahoo.fr>
2022-02-08 23:33:44 +0800

    Added BaseIcon

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseIcon from "@/components/BaseIcon.vue";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .component("BaseIcon", BaseIcon)
+  .mount("#app");
