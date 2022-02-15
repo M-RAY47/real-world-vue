3742ac2076fef04721da5e6318b0498967cdf054
M.RAY <a.mray47@yahoo.fr>
2022-02-15 23:18:57 +0800

    Added lodash

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
-import upperFirst from "lodash/upperFirst"
-import camelCase from "lodash/camelCase"
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import BaseIcon from "@/components/BaseIcon.vue";
+
 
 createApp(App)
   .use(store)
--- src/services/EventService.js
+++ src/services/EventService.js
@@ -4,10 +4,10 @@ const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
   withCredentials: false, // This is the default
   headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
-})
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
 
 export default {
   getEvents() {
