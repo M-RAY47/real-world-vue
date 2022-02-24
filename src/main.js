9edd7154ada66bb6702fb410680cfbb8a5f5d946
M.RAY <a.mray47@yahoo.fr>
2022-02-24 23:57:31 +0800

    modified the global vue components

--- src/main.js
+++ src/main.js
@@ -18,8 +18,9 @@ requireComponent.keys().forEach((fileName) => {
     camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
   );
 
-  Vue.component(componentName, componentConfig.default || componentConfig);
+  // Vue.component(componentName, componentConfig.default || componentConfig);
 });
+Vue.component(componentName, componentConfig.default || componentConfig);
 
 new Vue({
   router,
