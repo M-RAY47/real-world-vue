299855812855509ea45cdf5dbabf68b5bfad7b29
M.RAY <a.mray47@yahoo.fr>
2022-03-05 23:27:15 +0800

    add style to event show page

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -39,11 +39,6 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-      event: {},
-    };
-  },
   created() {
     EventService.getEvent(this.id)
       .then((res) => {
@@ -56,4 +51,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header {
+  margin: 0;
+}
+.event-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.event-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
