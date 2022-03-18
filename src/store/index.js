3ae8fae8350d1ebc81763655d7f1ef29bf363b57
M.RAY <a.mray47@yahoo.fr>
2022-03-18 23:34:34 +0800

    add state for event create

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    user: {
+      id: "abcd123",
+      name: "M.RAY",
+    }
+  },
   mutations: {},
   actions: {},
   modules: {},
