3e0fa75cdb8d1d44f89431a60240c64f02656123
M.RAY <a.mray47@yahoo.fr>
2022-03-21 23:58:56 +0800

    clean the create event component

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <h1>Create an Event, {{ user.name }}</h1>
-    <p>The is the id: {{ user.id }}</p>
-    <ul>
-      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
-    </ul>
-    <p>The total categories are {{ catLength }}</p>
   </div>
 </template>
 
@@ -14,9 +9,6 @@ import { mapState, mapGetters } from "vuex";
 
 export default {
   computed: {
-    localCatLength() {
-      return this.$store.getters.catLength;
-    },
     ...mapGetters(["catLength"]),
     ...mapState(["user", "categories"]),
   },
