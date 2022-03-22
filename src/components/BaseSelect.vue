4b74e8ebcd553f6ab98c7811d66db4af46085365
M.RAY <a.mray47@yahoo.fr>
2022-03-22 22:16:37 +0800

    add BaseSelect component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label></label>
+    <select>
+      <option
+
+      ></option>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
