fbd1f89de6e7a1995ba43b1d4f51de51582c5a28
M.RAY <a.mray47@yahoo.fr>
2022-03-29 00:05:44 +0800

    storing the number of events in to limit the displaying

--- src/store/index.js
+++ src/store/index.js
@@ -39,7 +39,6 @@ export default createStore({
     fetchEvents({ commit }, { perPage, page }) {
       return EventService.getEvents(perPage, page)
         .then((res) => {
-          console.log("This is the total:", res.headers["x-total-count"]);
           const total = res.headers["x-total-count"];
           commit("SET_TOTAL_EVENTS", total);
           commit("SET_EVENTS", res.data);
