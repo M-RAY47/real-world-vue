7524702724592c1f25dc772c14988b15210db1a0
M.RAY <a.mray47@yahoo.fr>
2022-04-07 23:06:56 +0800

    fix another bug

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "vuex";
 import EventService from "@/services/EventService.js";
-import * as user from "@stort/modules/user.js";
+import * as user from "@store/modules/user.js";
 
 export default createStore({
   modules: {
