29a8c487da11f8e90320deeae2f34112f04792f8
M.RAY <a.mray47@yahoo.fr>
2022-04-10 01:06:33 +0800

    fix typo

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "vuex";
 import * as user from "@/store/modules/user.js";
-import * as event from "@store/modules/event.js";
+import * as event from "@/store/modules/event.js";
 
 export default createStore({
   modules: {
