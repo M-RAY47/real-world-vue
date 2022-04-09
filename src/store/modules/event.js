d88c13d816ee128b74e6c767c98de0a8691c301d
M.RAY <a.mray47@yahoo.fr>
2022-04-09 23:32:38 +0800

    prettier errors fixed event module

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -1,69 +1,70 @@
 import EventService from "@services/EventService.js";
+
 export const state = {
-	event: {},
-	events: [],
-	categories: [
-		"sustainability",
-		"natural",
-		"animal welfare",
-		"housing",
-		"eduction",
-		"food",
-		"community",
-	],
-	totalEvents: null,
+  event: {},
+  events: [],
+  categories: [
+    "sustainability",
+    "natural",
+    "animal welfare",
+    "housing",
+    "eduction",
+    "food",
+    "community",
+  ],
+  totalEvents: null,
 };
 
-export const  mutations = {
-	ADD_EVENT(state, event) {
-		state.events.push(event);
-	},
-	SET_EVENTS(state, events) {
-		state.events = events;
-	},
-	SET_TOTAL_EVENTS(state, total) {
-		state.totalEvents = total;
-	},
-	SET_EVENT(state, event) {
-		state.event = event;
-	},
+export const mutations = {
+  ADD_EVENT(state, event) {
+    state.events.push(event);
+  },
+  SET_EVENTS(state, events) {
+    state.events = events;
+  },
+  SET_TOTAL_EVENTS(state, total) {
+    state.totalEvents = total;
+  },
+  SET_EVENT(state, event) {
+    state.event = event;
+  },
 };
 
 export const actions = {
-	createEvent({ commit }, event) {
-		return EventService.postEvent(event).then(() => {
-			commit("ADD_EVENT", event);
-		});
-	},
-	fetchEvents({ commit }, { perPage, page }) {
-		return EventService.getEvents(perPage, page)
-			.then((res) => {
-				const total = parseInt(res.headers["x-total-count"]);
-				commit("SET_TOTAL_EVENTS", total);
-				commit("SET_EVENTS", res.data);
-			})
-			.catch((err) => {
-				console.log("This is the error:" + err.message);
-			});
-	},
-	fetchEvent({ commit, getters, state }, id) {
-		if (id == state.event.id) {
-			return state.event;
-		}
+  createEvent({ commit }, event) {
+    return EventService.postEvent(event).then(() => {
+      commit("ADD_EVENT", event);
+    });
+  },
+  fetchEvents({ commit }, { perPage, page }) {
+    return EventService.getEvents(perPage, page)
+      .then((res) => {
+        const total = parseInt(res.headers["x-total-count"]);
+        commit("SET_TOTAL_EVENTS", total);
+        commit("SET_EVENTS", res.data);
+      })
+      .catch((err) => {
+        console.log("This is the error:" + err.message);
+      });
+  },
+  fetchEvent({ commit, getters, state }, id) {
+    if (id == state.event.id) {
+      return state.event;
+    }
 
-		var event = getters.getEventByIds(id);
-		if (event) {
-			commit("SET_EVENT", event);
-		} else {
-			return EventService.getEvent(id)
-				.then((res) => {
-					commit("SET_EVENT", res.data);
-				})
-				.catch((err) => {
-					console.log("This is the err:" + err.message);
-				});
-		}
-	},
+    var event = getters.getEventByIds(id);
+    if (event) {
+      commit("SET_EVENT", event);
+    } else {
+      return EventService.getEvent(id)
+        .then((res) => {
+          commit("SET_EVENT", res.data);
+        })
+        .catch((err) => {
+          console.log("This is the err:" + err.message);
+        });
+    }
+  },
 };
 
 export const getters = {
