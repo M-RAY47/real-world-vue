0780c5cef2588947a1c3d2f05d66ff30f70b082c
M.RAY <a.mray47@yahoo.fr>
2022-04-16 00:18:48 +0800

    added notification bar

---
+++ src/components/NotificationContainer.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="notification-bar">
+		<p>{{ notifiction.message }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	props: { 
+		notifiction: {
+			type: Object,
+			required: true,
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.notifiction-bar {
+  margin: 1em 0 1em;
+}
+</style>
