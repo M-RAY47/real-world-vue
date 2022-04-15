79db4ff5e94a77ea585b41a2cdf20131463d58a1
M.RAY <a.mray47@yahoo.fr>
2022-04-15 23:59:44 +0800

    created succcess notification after an event successefully created

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -23,20 +23,22 @@ export const mutations = {
 
 export const actions = {
   createEvent({ commit, dispatch }, event) {
-    return EventService.postEvent(event).then(() => {
-      commit("ADD_EVENT", event);
-      const notification = {
-        type: "success",
-        message: "Your event was successfully created"! 
-      }
-      dispatch("notifictions/add", notification, {root: true});
-    }).catch((err) => {
-      const notification = {
-        type: "error",
-        message: "There is problem creating event" + err.message 
-      };
-    dispatch("notifictions/add", notification, {root: true});
-    });
+    return EventService.postEvent(event)
+      .then(() => {
+        commit("ADD_EVENT", event);
+        const notification = {
+          type: "success",
+          message: "Your event was successfully created!",
+        };
+        dispatch("notifictions/add", notification, { root: true });
+      })
+      .catch((err) => {
+        const notification = {
+          type: "error",
+          message: "There is problem creating event" + err.message 
+        };
+        dispatch("notifictions/add", notification, {root: true});
+      });
   },
   fetchEvents({ commit, dispatch }, { perPage, page }) {
     return EventService.getEvents(perPage, page)
