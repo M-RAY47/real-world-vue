e09ecd6018b647aebbfe7f39d928380159e972b5
M.RAY <a.mray47@yahoo.fr>
2022-04-16 01:17:42 +0800

    added auto delete notification

--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -5,18 +5,32 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
+  data() {
+    return {
+      timeout: null,
+    };
+  },
   props: {
     notification: {
       type: Object,
       required: true,
     },
   },
+  mounted() {
+    this.timeout = setTimeout(() => this.remove(this.notification), 5000);
+  },
+  beforeUnmount() {
+    clearTimeout(this.timeout);
+  },
   computed: {
     notificationTypeClass() {
       return `-text-${this.notification.type}`;
     },
   },
+  methods: mapActions("notifications", ["remove"]),
 };
 </script>
 
