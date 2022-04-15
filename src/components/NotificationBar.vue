fb16d91c2955943a5e4641c47ecba0e7e174a767
M.RAY <a.mray47@yahoo.fr>
2022-04-16 00:24:52 +0800

    added class for notification bar

---
+++ src/components/NotificationBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="notification-bar">
+    <p>{{ notifiction.message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notifiction: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    notificationTypeClass () {
+      return `-text-${this.notifiction.type}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.notifiction-bar {
+  margin: 1em 0 1em;
+}
+</style>
