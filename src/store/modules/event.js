223476fb6643b74aaeeacee54fe59b4510d7072c
M.RAY <a.mray47@yahoo.fr>
2022-04-16 01:20:37 +0800

    fix notification message

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -35,7 +35,7 @@ export const actions = {
       .catch((err) => {
         const notification = {
           type: "error",
-          message: "There is problem creating event" + err.message,
+          message: "There is problem creating event " + err.message,
         };
         dispatch("notifications/add", notification, { root: true });
         throw err;
