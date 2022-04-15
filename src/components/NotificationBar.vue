ae6ed3108559235e64f5d2863c422844d17183a9
M.RAY <a.mray47@yahoo.fr>
2022-04-16 00:58:30 +0800

    added notification bar component

--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="notification-bar">
-    <p>{{ notifiction.message }}</p>
+  <div class="notification-bar" :class="notificationTypeClass">
+    <p>{{ notification.message }}</p>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    notifiction: {
+    notification: {
       type: Object,
       required: true,
     },
   },
   computed: {
-    notificationTypeClass () {
-      return `-text-${this.notifiction.type}`;
+    notificationTypeClass() {
+      return `-text-${this.notification.type}`;
     },
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.notifiction-bar {
+<style scoped>
+.notification-bar {
   margin: 1em 0 1em;
 }
 </style>
