9161eb46ec946b3a6d9d34449baba3e67a4bac43
M.RAY <a.mray47@yahoo.fr>
2022-04-18 21:55:19 +0800

    created base Input component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<label v-if="label">{{ label }}</label>
+		<input :value="value" @input="updateValue" v-bind="$attrs" v-on="listeners"
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
