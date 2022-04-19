1139d2c786c31b335aa23b28c0ea365706f64563
M.RAY <a.mray47@yahoo.fr>
2022-04-20 01:38:27 +0800

    added nprogress for api calls

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import camelCase from "lodash/camelCase";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "nprogress/nprogress.css";
 
 const app = createApp(App);
 const requireComponent = require.context(
