2cd795542841bfdfbb3c892cf9cb672929a7f56a
M.RAY <a.mray47@yahoo.fr>
2022-04-24 20:28:46 +0800

    added perPage in the store module

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -4,6 +4,7 @@ export const state = {
   event: {},
   events: [],
   totalEvents: null,
+  perPage: 3,
 };
 
 export const mutations = {
@@ -41,8 +42,8 @@ export const actions = {
         throw err;
       });
   },
-  fetchEvents({ commit, dispatch }, { perPage, page }) {
-    return EventService.getEvents(perPage, page)
+  fetchEvents({ commit, dispatch, state }, { page }) {
+    return EventService.getEvents(state.perPage, page)
       .then((res) => {
         const total = parseInt(res.headers["x-total-count"]);
         commit("SET_TOTAL_EVENTS", total);
