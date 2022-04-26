24b1a4b016cf293600553f508a4bf5cefa85a28b
M.RAY <a.mray47@yahoo.fr>
2022-04-26 22:43:14 +0800

    created notfound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>Oop!</h1>
+		<h2>The page you are looing for is not here!</h2>
+    <route-link :to="{ name: 'event-list' }"
+      >Back to the list of events</route-link
+    >
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
