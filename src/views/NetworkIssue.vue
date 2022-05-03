958645e8801751452947725e008e7c43353bd5ed
M-RAY47 <a.mray47@yahoo.fr>
2022-05-03 23:22:55 +0800

    fixed typo

--- src/views/NetworkIssue.vue
+++ src/views/NetworkIssue.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Uh-oh!</h1>
     <h3>
-      Ilooks like you're experiencing some network issues, please click the back
+      It looks like you're experiencing some network issues, please click the back
       button and try again!
     </h3>
     <router-link :to="{ name: 'event-list' }"
