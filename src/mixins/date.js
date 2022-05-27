1150d650b517b885e3df3f32069755b54d244006
M-RAY47 <a.mray47@yahoo.fr>
2022-05-27 22:22:14 +0800

    created computed date filter

---
+++ src/mixins/date.js
@@ -0,0 +1,12 @@
+export const dateMixins = {
+  computed: {
+    date(value) {
+      const date = new Date(value);
+      return date.toLocaleString(["en-US"], {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      });
+    },
+  },
+};
