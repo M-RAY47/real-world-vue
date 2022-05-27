c378a0b158d0774e4775d9501290d6eaf654246f
M-RAY47 <a.mray47@yahoo.fr>
2022-05-27 22:21:40 +0800

    registered DateFilter globaly

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "nprogress/nprogress.css";
-import DateFilter from "./filters/date";
+import DateFilter from "./mixins/date";
 
 const app = createApp(App);
 const requireComponent = require.context(
